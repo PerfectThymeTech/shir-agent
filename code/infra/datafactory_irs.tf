b3c7b546765c8308ad939bbcff6a1e681ca54627
Marvin Buss <marvin.buss@gmail.com>
2024-03-23 20:27:57 +0100

    Add Missing Dependencies

--- code/infra/datafactory_irs.tf
+++ code/infra/datafactory_irs.tf
@@ -3,4 +3,8 @@ resource "azurerm_data_factory_integration_runtime_self_hosted" "data_factory_in
   data_factory_id = azurerm_data_factory.data_factory.id
 
   description = "Self-Hosted Integration Runtime 001"
+
+  depends_on = [
+    azurerm_private_endpoint.data_factory_private_endpoint
+  ]
 }
