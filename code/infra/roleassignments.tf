5d287be7d59a294dd6d9a4e150e48e986ebd5ede
Marvin Buss <marvin.buss@gmail.com>
2024-09-30 10:18:47 +0200

    Remove role assignment which is no longer required

--- code/infra/roleassignments.tf
+++ code/infra/roleassignments.tf
@@ -1,12 +1,3 @@
-# Current
-resource "azurerm_role_assignment" "current_role_assignment_key_vault_administrator" {
-  description          = "Required to create secrets in Key Vault."
-  scope                = module.key_vault.key_vault_id
-  role_definition_name = "Key Vault Administrator"
-  principal_id         = data.azurerm_client_config.current.object_id
-  principal_type       = "ServicePrincipal"
-}
-
 # User Assigned Identity
 resource "azurerm_role_assignment" "uai_role_assignment_key_vault_secrets_user" {
   description          = "Required for Web App to access secrets via Key Vault references."
